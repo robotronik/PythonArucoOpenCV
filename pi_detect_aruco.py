19a58933cd4ee1ddbde715d4d2825072452b521d
stefguimove <salut.bonjou@gmail.com>
2026-05-15 09:15:33 +0200

    no filtering buggy

--- pi_detect_aruco.py
+++ pi_detect_aruco.py
@@ -272,7 +272,7 @@ def extract_aruco(frame, mtx, dist, aruco_dict, aruco_params, headless, showReje
                 if marker_id in gameobject:
                     
                     # --- FILTRE DES HAUTEURS ---
-                    if camera_position[2] >= 45:
+                    if False and camera_position[2] >= 45:
                         continue # On ignore les tags à plus de 45mm du sol
                     # --- FILTRE DES FACES LATÉRALES ---
                     # On vérifie l'alignement de l'axe Z du marqueur avec l'axe Z de la caméra.
